851a8dd46c7b2ef0a7895bdc67388a436a18a33d
speedevil123 <133794554+speedevil123@users.noreply.github.com>
2024-02-26 18:35:59 +0700

    Add files via upload

---
+++ la8/la8/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace la8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            bindingSource1 = new BindingSource(components);
+            textBox5 = new TextBox();
+            button1 = new Button();
+            textBox6 = new TextBox();
+            label6 = new Label();
+            listBox1 = new ListBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridView1.EditMode = DataGridViewEditMode.EditOnEnter;
+            dataGridView1.Location = new Point(28, 316);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Control;
+            dataGridViewCellStyle3.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.ShowEditingIcon = false;
+            dataGridView1.Size = new Size(431, 215);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellClick += dataGridView1_CellContentClick;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            dataGridView1.RowHeaderMouseClick += dataGridView1_RowHeaderMouseClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(346, 292);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Расписание";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ButtonHighlight;
+            textBox1.Location = new Point(126, 32);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(333, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ButtonHighlight;
+            textBox2.Location = new Point(126, 98);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(333, 27);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ButtonHighlight;
+            textBox3.Location = new Point(126, 164);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(333, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ButtonHighlight;
+            textBox4.Location = new Point(126, 230);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(333, 27);
+            textBox4.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(12, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 18);
+            label2.TabIndex = 6;
+            label2.Text = "День недели";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(43, 107);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 18);
+            label3.TabIndex = 7;
+            label3.Text = "Предмет";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(28, 168);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 18);
+            label4.TabIndex = 8;
+            label4.Text = "Аудитория";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(64, 234);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 18);
+            label5.TabIndex = 9;
+            label5.Text = "Время";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(28, 283);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(161, 27);
+            textBox5.TabIndex = 10;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.GradientInactiveCaption;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(195, 283);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 27);
+            button1.TabIndex = 12;
+            button1.Text = "Поиск";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = SystemColors.ButtonHighlight;
+            textBox6.Location = new Point(529, 32);
+            textBox6.Name = "textBox6";
+            textBox6.ReadOnly = true;
+            textBox6.Size = new Size(203, 27);
+            textBox6.TabIndex = 13;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Tahoma", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(529, 9);
+            label6.Name = "label6";
+            label6.Size = new Size(108, 18);
+            label6.TabIndex = 14;
+            label6.Text = "День недели";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(529, 87);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(630, 444);
+            listBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1182, 553);
+            Controls.Add(listBox1);
+            Controls.Add(label6);
+            Controls.Add(textBox6);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "XML-обработчик";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private BindingSource bindingSource1;
+        private TextBox textBox5;
+        private Button button1;
+        private TextBox textBox6;
+        private Label label6;
+        private ListBox listBox1;
+    }
+}
---
+++ la8/la8/Form1.cs
@@ -0,0 +1,97 @@
+using System.Data;
+using System.Xml;
+
+namespace la8
+{
+    public partial class Form1 : Form
+    {
+        string file_name = "raspisanie.xml";
+        XmlTextReader xml_read;
+        DataSet DataXML;
+        DataTable MyDatatable = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataXML = new DataSet();
+            FileStream fs = new FileStream(file_name, FileMode.Open);
+            xml_read = new XmlTextReader(fs);
+            DataXML.ReadXml(xml_read, XmlReadMode.InferSchema);
+
+            MyDatatable = DataXML.Tables[0];
+            bindingSource1.DataMember = DataXML.Tables[0].ToString();
+            bindingSource1.DataSource = DataXML.Tables[0];
+
+            dataGridView1.DataSource = bindingSource1;
+            dataGridView1.EditMode = DataGridViewEditMode.EditOnEnter;
+
+            dataGridView1.Columns[0].Width = 110;
+            dataGridView1.Columns[0].HeaderText = "День недели";
+
+            dataGridView1.Columns[1].Width = 100;
+            dataGridView1.Columns[1].HeaderText = "Предмет";
+
+            dataGridView1.Columns[2].Width = 100;
+            dataGridView1.Columns[2].HeaderText = "Аудитория";
+
+            dataGridView1.Columns[3].Width = 100;
+            dataGridView1.Columns[3].HeaderText = "Время занятия";
+
+            ;
+            fs.Close();
+            dataGridView1.ClearSelection();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                textBox1.Text = DataXML.Tables[0].Rows[e.RowIndex][0].ToString();
+                textBox2.Text = DataXML.Tables[0].Rows[e.RowIndex][1].ToString();
+                textBox3.Text = DataXML.Tables[0].Rows[e.RowIndex][2].ToString();
+                textBox4.Text = DataXML.Tables[0].Rows[e.RowIndex][3].ToString();
+            }
+            else
+                return;
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            textBox1.Text = DataXML.Tables[0].Rows[e.RowIndex][0].ToString();
+            textBox2.Text = DataXML.Tables[0].Rows[e.RowIndex][1].ToString();
+            textBox3.Text = DataXML.Tables[0].Rows[e.RowIndex][2].ToString();
+            textBox4.Text = DataXML.Tables[0].Rows[e.RowIndex][3].ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == null)
+            {
+                MessageBox.Show("Введите значение!");
+                return;
+            }
+
+            for (int i = 0; i < DataXML.Tables[0].Rows.Count; i++)
+            {
+                if (DataXML.Tables[0].Rows[i][0].ToString() == textBox5.Text)
+                {
+                    textBox6.Text = textBox5.Text;
+                    string line = "Предмет: " + DataXML.Tables[0].Rows[i][1].ToString() + ", Аудитория: " +
+                        DataXML.Tables[0].Rows[i][2].ToString() + ", Время: " + DataXML.Tables[0].Rows[i][3].ToString();
+                    listBox1.Items.Add(line);
+                }
+            }
+
+        }
+    }
+}
